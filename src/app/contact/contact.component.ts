5d026ca6fa48ac1f92968858dc0d286ef4c6cb76
POFIGISTER <mr.nikiti81@gmail.com>
2023-02-09 16:27:08 +0200

    clean code

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,10 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
-  /*position = {
-    lat: 48.467803,
-    lng: 35.049846,
-  }*/
 
   center: google.maps.LatLngLiteral = {
     lat: 48.467803,
@@ -20,30 +16,6 @@ export class ContactComponent {
     lat: 48.467803,
     lng: 35.049846,
   }
-  /*options: google.maps.MapOptions = {
-    mapTypeId: 'hybrid',
-    zoomControl: false,
-    scrollwheel: false,
-    disableDoubleClickZoom: true,
-    maxZoom: 15,
-    minZoom: 8,
-  };*/
-
-
-  /*addMarker() {
-    this.marker.push({
-      position: {
-        lat: 48.467803,
-        lng: 35.049846,
-      },
-      label: {
-        color: 'red',
-        text: 'Marker label ' + (this.marker.length + 1),
-      },
-      title: 'Marker title ' + (this.marker.length + 1),
-      options: { animation: google.maps.Animation.BOUNCE },
-    });
-  }*/
 
 
 }
