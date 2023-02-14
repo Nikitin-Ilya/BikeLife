2f4d4f5cd38a5222c28600c668fcca0c638d7981
POFIGISTER <mr.nikiti81@gmail.com>
2023-02-14 16:52:16 +0200

    add component with list of products

---
+++ src/app/home/products-list/products-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { DataService, IProduct } from '../../services/data.service';
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.scss']
+})
+export class ProductsListComponent  implements OnInit{
+  products!: IProduct[];
+
+  constructor(private dataService: DataService){}
+
+  ngOnInit(): void {
+    this.dataService.getProducts().subscribe(response=>{
+      this.products=response
+    })
+  }
+}
