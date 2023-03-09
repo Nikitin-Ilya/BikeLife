38abd9c039f46ae58e8fbb7ab7b923606ae0d2a0
POFIGISTER <mr.nikiti81@gmail.com>
2023-03-09 15:33:41 +0200

    create form for add new product

--- src/app/add-new-product/add-new-product.component.ts
+++ src/app/add-new-product/add-new-product.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {FormControl} from '@angular/forms';
+import {MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+import {MatChipInputEvent} from '@angular/material/chips';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import { FormGroup, Validators} from '@angular/forms';
+import {IProduct} from '../services/data.service';
 
 @Component({
   selector: 'app-add-new-product',
@@ -6,5 +14,88 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-new-product.component.scss']
 })
 export class AddNewProductComponent {
+  product!: IProduct;
 
+  productForm: FormGroup = new FormGroup({
+    name: new FormControl("", [Validators.required]),
+    shop: new FormControl("", [Validators.required]),
+    description: new FormControl("", [Validators.required]),
+    price: new FormControl("", [Validators.required]),
+    discount: new FormControl("", [Validators.required]),
+    size: new FormControl("", [Validators.required]),
+    colors: new FormControl("", [Validators.required]),
+    discountUntil: new FormControl("", [Validators.required]),
+    new: new FormControl(""),
+    shipping: new FormControl(""),
+    main: new FormControl(""),
+  })
+
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  colorCtrl:FormControl = new FormControl('');
+  filteredselectedColors?: Observable<string[]>;
+  selectedColors: string[] = [];
+  defaultColors: string[] = ['Red', 'Green', 'Blue', 'Yellow', 'Orange', 'Black', 'White', 'Pink'];
+  availableSizes: string[] = ['XXS', 'XS', 'S', 'M', 'L', 'XL', 'XXL'];
+  uploadedFiles: File[] = [];
+
+  @ViewChild('colorInput') colorInput!: ElementRef<HTMLInputElement>;
+
+  previewWidth: number = 250;
+
+  constructor() {
+    this.filteredselectedColors = this.colorCtrl.valueChanges.pipe(
+      startWith(null),
+      map((color: string | null) => (color ? this._filter(color) : this.defaultColors.slice())),
+    );
+  }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our color
+    if (value) {
+      this.selectedColors.push(value);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+
+    this.colorCtrl.setValue(null);
+  }
+
+  remove(fruit: string): void {
+    const index = this.selectedColors.indexOf(fruit);
+
+    if (index >= 0) {
+      this.selectedColors.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.selectedColors.push(event.option.viewValue);
+    this.colorInput.nativeElement.value = '';
+    this.colorCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.defaultColors.filter(color => color.toLowerCase().includes(filterValue));
+  }
+
+  datePickerFilter = (d: Date | null): boolean => {
+    const day = d || new Date();
+    const dateNow = new Date();
+    dateNow.setDate(dateNow.getDate() - 1);
+    return day > dateNow;
+  };
+
+  onUpload(event: any) {
+    for(let file of event.files) {
+      this.uploadedFiles.push(file);
+    }
+  }
+
+  onSubmit():void {
+    alert('New product added successfully')
+  }
 }
--- src/app/add-new-product/add-new-product.module.ts
+++ src/app/add-new-product/add-new-product.module.ts
@@ -4,6 +4,21 @@ import { CommonModule } from '@angular/common';
 import { AddNewProductRoutingModule } from './add-new-product-routing.module';
 import { AddNewProductComponent } from './add-new-product.component';
 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatSelectModule} from '@angular/material/select';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatIconModule} from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import {MatButtonModule} from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FileUploadModule} from 'primeng/fileupload';
+import {HttpClientModule} from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
@@ -11,7 +26,21 @@ import { AddNewProductComponent } from './add-new-product.component';
   ],
   imports: [
     CommonModule,
-    AddNewProductRoutingModule
+    AddNewProductRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatChipsModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    FileUploadModule,
+    HttpClientModule
   ]
 })
 export class AddNewProductModule { }
